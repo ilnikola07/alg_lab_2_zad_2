107efc53d3bbd5560a7ba113e17010cea655a628
ilnikola07 <nikilushin07@gmail.com>
2026-02-28 01:14:38 +0300

    Add files via upload
    файлы с тестами

---
+++ NumberProcessorTests.cs
@@ -0,0 +1,11 @@
+﻿//namespace NumberLogicTest
+//{
+//    [TestClass]
+//    public sealed class Test1
+//    {
+//        [TestMethod]
+//        public void TestMethod1()
+//        {
+//        }
+//    }
+//}
